e7358c90b779e73eeffd2565c6a1030a8c438eaf
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2021-09-27 17:47:43 +0530

    Add files via upload

---
+++ Notes-WebApp/app.js
@@ -0,0 +1,88 @@
+showNotes();
+// Add notes to the local storage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+  console.log(notesObj);
+  showNotes();
+  location.reload();
+});
+
+function success() {
+  if ((document.getElementById("addTxt").value === "") || (document.getElementById("addTitle").value === "")) {
+    document.getElementById("addBtn").disabled = true;
+  } else {
+    document.getElementById("addBtn").disabled = false;
+  }
+}
+
+// Showing Notes from local storage
+function showNotes() {
+  notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html =
+      html +
+      `<div class="noteCard card my-2 mx-2" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text">${element.text}</p>
+            <button id="${index}" onclick="deleteNotes(this.id)" class="btn btn-primary">Delete</button>
+          </div>
+        </div>`;
+  });
+  let savedNotes = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    savedNotes.innerHTML = html;
+  } else {
+    savedNotes.innerHTML = `<span>You have not saved any notes, use <b><i>"Add a note"</i></b> section to add notes</span>`;
+  }
+}
+
+// Delete notes from storage
+function deleteNotes(index) {
+  notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+// Searching a note
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
