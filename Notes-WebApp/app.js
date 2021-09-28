71fb73575212a4ce7caad16981fe7f919fa412f3
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2021-09-28 13:46:10 +0530

    Updated app.js

--- Notes-WebApp/app.js
+++ Notes-WebApp/app.js
@@ -86,3 +86,28 @@ search.addEventListener("input", function () {
     }
   });
 });
+
+document.addEventListener('keydown', function () {
+    if (event.keyCode == 123) {
+        alert("This function has been disabled to prevent from viewing code!");
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+        alert("This function has been disabled to prevent from viewing code!");
+        return false;
+    } else if (event.ctrlKey && event.keyCode == 85) {
+        alert("This function has been disabled to prevent from viewing code!");
+        return false;
+    }
+}, false);
+
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function (e) {
+        alert("This function has been disabled to prevent from viewing code!");
+        e.preventDefault();
+    }, false);
+} else {
+    document.attachEvent('oncontextmenu', function () {
+        alert("This function has been disabled to prevent from viewing code!");
+        window.event.returnValue = false;
+    });
+}
