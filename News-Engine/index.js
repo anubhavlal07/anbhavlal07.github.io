447e02347f4717884a570f7e4ed95fed6f5c08f4
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2021-09-30 23:13:04 +0530

    Added time stamp on news engine

--- News-Engine/index.js
+++ News-Engine/index.js
@@ -3,17 +3,21 @@ let newsAccordion = document.getElementById("newsAccordion");
 
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
-xhr.open("GET", `https://gnews.io/api/v4/top-headlines?country=in&lang=en&token=${apiKey}`, true);
+xhr.open(
+  "GET",
+  `https://gnews.io/api/v4/top-headlines?country=in&lang=en&token=${apiKey}`,
+  true
+);
 
 // What to do when response is ready
 xhr.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let articles = json.articles;
-        let newsHtml = "";
-        articles.forEach(function (element, index) {
-            console.log(element, index)
-            let news = `<div class="accordion-item">
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    let newsHtml = "";
+    articles.forEach(function (element, index) {
+      console.log(element, index);
+      let news = `<div class="accordion-item">
                             <h2 class="accordion-header" id="flush-heading${index}">
                                 <button class="accordion-button collapsed bg-info text-white" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
@@ -25,37 +29,59 @@ xhr.onload = function () {
                                 <div class="accordion-body">${element.description} — ${element.source.name} <br> <a href="${element.url}" target="_blank"> Read Full Article </a></div>
                             </div>
                         </div>`;
-            newsHtml += news;
-        });
-        newsAccordion.innerHTML = newsHtml;
-    } else {
-        console.log("Sorry Some Error Occured !");
-    }
+      newsHtml += news;
+    });
+    newsAccordion.innerHTML = newsHtml;
+  } else {
+    console.log("Sorry Some Error Occured !");
+  }
 };
 
 xhr.send();
 
-document.addEventListener('keydown', function () {
+document.addEventListener(
+  "keydown",
+  function () {
     if (event.keyCode == 123) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
+      alert("This function has been disabled to prevent from copying content.");
+      return false;
     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
+      alert("This function has been disabled to prevent from copying content.");
+      return false;
     } else if (event.ctrlKey && event.keyCode == 85) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
+      alert("This function has been disabled to prevent from copying content.");
+      return false;
     }
-}, false);
+  },
+  false
+);
 
 if (document.addEventListener) {
-    document.addEventListener('contextmenu', function (e) {
-        alert("This function has been disabled to prevent from viewing code!");
-        e.preventDefault();
-    }, false);
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      alert("This function has been disabled to prevent from copying content.");
+      e.preventDefault();
+    },
+    false
+  );
 } else {
-    document.attachEvent('oncontextmenu', function () {
-        alert("This function has been disabled to prevent from viewing code!");
-        window.event.returnValue = false;
-    });
+  document.attachEvent("oncontextmenu", function () {
+    alert("This function has been disabled to prevent from copying content.");
+    window.event.returnValue = false;
+  });
 }
+
+setInterval(() => {
+  // To set a running time stamp in the page
+  // get a new date (locale machine date time)
+  var date = new Date();
+  // get the date as a string
+  var n = date.toDateString();
+  // get the time as a string
+  var time = date.toLocaleTimeString();
+
+  // find the html element with the id of time
+  // set the innerHTML of that element to the date a space the time
+  document.getElementById("time").innerHTML = n + ", " + time;
+}, 1000);
