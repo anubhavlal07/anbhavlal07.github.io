7d3516378b67ab408708e271c0bcc6bf7576ac49
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2022-01-24 15:50:36 +0530

    Update minimax_algo.js

--- Tic-Tac-Toe-AI/minimax_algo.js
+++ Tic-Tac-Toe-AI/minimax_algo.js
@@ -194,3 +194,34 @@ function minimax(newBoard, player) {
 
   return moves[bestMove];
 } // end of minimax func()
+// Disabled Input from keyboard
+(document.onkeydown = function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    return false;
+  }
+}),
+  false;
+
+if (document.addEventListener) {
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
+} else {
+  document.attachEvent("oncontextmenu", function () {
+    window.event.returnValue = false;
+  });
+}
