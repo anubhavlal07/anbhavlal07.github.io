bbbae21e67f24272d9cc660e27cbf8f360aaba1d
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2022-01-24 22:41:59 +0530

    Update and rename app.js to index.js

--- Notes-WebApp/index.js
+++ Notes-WebApp/index.js
@@ -87,27 +87,34 @@ search.addEventListener("input", function () {
   });
 });
 
-document.addEventListener('keydown', function () {
-    if (event.keyCode == 123) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
-    } else if (event.ctrlKey && event.keyCode == 85) {
-        alert("This function has been disabled to prevent from viewing code!");
-        return false;
-    }
-}, false);
+// Disabled Input from keyboard
+(document.onkeydown = function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    return false;
+  }
+}),
+  false;
 
 if (document.addEventListener) {
-    document.addEventListener('contextmenu', function (e) {
-        alert("This function has been disabled to prevent from viewing code!");
-        e.preventDefault();
-    }, false);
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
 } else {
-    document.attachEvent('oncontextmenu', function () {
-        alert("This function has been disabled to prevent from viewing code!");
-        window.event.returnValue = false;
-    });
+  document.attachEvent("oncontextmenu", function () {
+    window.event.returnValue = false;
+  });
 }
