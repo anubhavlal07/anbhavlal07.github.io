0d45549571aa967c4be74b5846b76b7d41a6f5a1
ANUBHAV LAL <67056859+anubhavlal07@users.noreply.github.com>
2022-01-24 22:43:34 +0530

    Update index.js

--- Tic-Tac-Toe/index.js
+++ Tic-Tac-Toe/index.js
@@ -133,3 +133,35 @@ resetDiv.addEventListener('click', handleReset);
 for (const cellDiv of cellDivs) {
   cellDiv.addEventListener('click', handleCellClick)
 }
+
+// Disabled Input from keyboard
+(document.onkeydown = function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    return false;
+  }
+}),
+  false;
+
+if (document.addEventListener) {
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
+} else {
+  document.attachEvent("oncontextmenu", function () {
+    window.event.returnValue = false;
+  });
+}
