c37d2ce2829ed6dbdd7d144df8a78a182dbd44a2
Anubhav Lal <67056859+anubhavlal07@users.noreply.github.com>
2023-01-26 18:52:55 +0530

    Updated some contents and fixed some style

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,33 +204,40 @@ scrollReveal.reveal(`.skillContent:nth-child(2)`, { origin: "right" });
 scrollReveal.reveal(`.qualificationContainer,.knowledgeCard`, {
   interval: 100,
 });
+
+// Dynamic year Footer
+let year = new Date().getFullYear();
+document.getElementById(
+  "footerCopy"
+).innerHTML = `Developed and Maintained by <a href="https://github.com/anubhavlal07" target="_blank">Anubhav Lal</a> <br> &copy; ${year} All Rights Reserved.`;
+
 // Diable input from users
-(document.onkeydown = function (event) {
-  if (event.keyCode == 123) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    return false;
-  }
-}),
-  false;
-if (document.addEventListener) {
-  document.addEventListener(
-    "contextmenu",
-    function (e) {
-      e.preventDefault();
-    },
-    false
-  );
-} else {
-  document.attachEvent("oncontextmenu", function () {
-    window.event.returnValue = false;
-  });
-}
+// (document.onkeydown = function (event) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+//     return false;
+//   } else if (event.ctrlKey && event.keyCode == 85) {
+//     return false;
+//   }
+// }),
+//   false;
+// if (document.addEventListener) {
+//   document.addEventListener(
+//     "contextmenu",
+//     function (e) {
+//       e.preventDefault();
+//     },
+//     false
+//   );
+// } else {
+//   document.attachEvent("oncontextmenu", function () {
+//     window.event.returnValue = false;
+//   });
+// }
