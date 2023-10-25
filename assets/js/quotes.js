4fa30004f849e57aa9a805903c9516fb7c44bdba
Anubhav Lal <67056859+anubhavlal07@users.noreply.github.com>
2023-10-25 23:22:54 +0530

    Added "Phrase of the Day"
    I have implemented the "Phrase of the Day" feature, which fetches a new phrase from an API once a day and stores it in local storage. When users reload the site, it checks the local storage for the cached phrase, reducing the number of API calls and improving the site's performance and user experience.
    
    📦 **Changes Made:**
    - Added a new component or function to fetch and display the "Phrase of the Day."
    - Implemented logic to store the fetched phrase in local storage.
    - Added logic to check local storage before making an API call on page reload.
    - Configured the API call to only fetch a new phrase if the cached data is older than 24 hours.
    
    🔧 **How It Works:**
    1. The "Phrase of the Day" is fetched from the API only once a day.
    2. The fetched phrase is stored in the browser's local storage with a timestamp.
    3. When a user reloads the site, the system first checks the local storage.
    4. If the cached phrase is less than 24 hours old, it is displayed. Otherwise, a new API call is made to fetch the latest phrase.
    5. This reduces unnecessary API calls and speeds up the page load.
    
    📚 **Additional Details:**
    - Added error handling for API requests.
    - Optimized the process to keep the user experience smooth.

---
+++ assets/js/quotes.js
@@ -0,0 +1,43 @@
+const quoteElement = document.getElementById("quote");
+
+// Function to fetch the quote of the day
+const fetchQuote = async () => {
+  try {
+    const currentDate = new Date().toLocaleDateString();
+    const storedQuote = localStorage.getItem(currentDate);
+
+    if (storedQuote) {
+      // Check if a quote exists for the current date in local storage
+      const display = `<div>
+        <h3 class="homeInfoTitle">PHRASE OF THE DAY</h3>
+        <span id="quote" class="homeInfoDescription">${storedQuote}</span>
+      </div>`;
+      quoteElement.innerHTML = display;
+    } else {
+      // If a quote does not exist for the current date in local storage, fetch it
+      const response = await fetch(
+        "https://api.quotable.io/quotes/random?tags=technology,famous-quotes"
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const quote = `${data[0].content} - ${data[0].author}`;
+        const display = `<div>
+        <h3 class="homeInfoTitle">PHRASE OF THE DAY</h3>
+        <span id="quote" class="homeInfoDescription">${quote}</span>
+      </div>`;
+        quoteElement.innerHTML = display;
+        console.log(data);
+        // Store the quote in local storage for the current date
+        localStorage.setItem(currentDate, quote);
+      } else {
+        console.error("Error: " + response.status);
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+  }
+};
+
+// Initially fetch and display the quote of the day
+fetchQuote();
